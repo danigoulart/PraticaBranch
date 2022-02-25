75d0f5370633651b118eab2cd959f166945e3a0f
Leonardo <leodscarvalho@gmail.com>
2022-02-25 11:38:17 -0300

    adicionado

--- src/LeoTeste/LeoTeste.java
+++ src/LeoTeste/LeoTeste.java
@@ -5,6 +5,7 @@ public class LeoTeste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("teste");
+		System.out.println("test");
 	}
 
 }
---
+++ src/LeoTeste/testezinho.java
@@ -0,0 +1,10 @@
+package LeoTeste;
+
+public class testezinho {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TESTANDOOO");
+	}
+
+}
